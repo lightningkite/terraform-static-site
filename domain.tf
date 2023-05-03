f0c05e1a13315b3610a140cf2cd180ca3abeeb22
Joseph Ivie <joseph@lightningkite.com>
2023-05-03 17:10:38 -0600

    Origin config modified to support index.html in subfolders

--- domain.tf
+++ domain.tf
@@ -67,17 +67,19 @@ resource "aws_cloudfront_distribution" "main" {
   default_root_object = "index.html"
 
   origin {
-    domain_name = aws_s3_bucket.files.bucket_regional_domain_name
-    origin_id   = aws_s3_bucket.files.id
-
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    domain_name = aws_s3_bucket.files.website_endpoint
+    origin_id   = "origin-${var.domain_name}"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = ["TLSv1"]
     }
   }
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
     cached_methods         = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id       = aws_s3_bucket.files.id
+    target_origin_id       = "origin-${var.domain_name}"
     viewer_protocol_policy = "redirect-to-https" # other options - https only, http
 
     forwarded_values {
